0959c5c5b81b86f194bfd7c391879b8bc3f81d2a
Stijn Van Hulle <stijn@stijnvanhulle.be>
2022-01-02 13:03:05 +0100

    fix: standard-version

--- .github/version.sh
+++ .github/version.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # sh ./version.sh bump => bumping version with standardVersion and will use patch as relaseVersion
-# sh ./version.sh bump major => bumping version with standardVersion and will use major as relaseVersion
 
 VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
 PACKAGE_NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
@@ -9,24 +8,18 @@ PACKAGE_NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' |
 PREVIOUS_VERSION=$(npm view $PACKAGE_NAME version)
 
 BUMP=$1
-PRE_RELEASE=$2
-
-if [ -z "$PRE_RELEASE" ]; then
-  PRE_RELEASE="patch"
-fi
-
 if [ -z "$BUMP" ]; then
   echo 'No BUMP found'
   exit 0
 fi
 
 if [ "$VERSION" == "$PREVIOUS_VERSION" ]; then
-  yarn run release:$PRE_RELEASE
+  yarn run release
   VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
 
-  echo "Release version: $VERSION, previous version: $PREVIOUS_VERSION with preRelease $PRE_RELEASE"
+  echo "Release version: $VERSION, previous version: $PREVIOUS_VERSION"
 else
-  yarn run release
+  yarn run release --skip.bump,changelog
 fi
 
 echo "VERSION=$VERSION" >>$GITHUB_ENV
